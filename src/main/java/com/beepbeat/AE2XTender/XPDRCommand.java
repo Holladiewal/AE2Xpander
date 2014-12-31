0a030bcc001c0255794a86cd26ec6e9ed2ef53a2
beepbeat <zfireman885@yahoo.de>
2014-12-31 12:37:47 +0100

    =Commit 2, stable, running but not working as intended

--- src/main/java/com/beepbeat/AE2XTender/XPDRCommand.java
+++ src/main/java/com/beepbeat/AE2XTender/XPDRCommand.java
@@ -3,7 +3,6 @@ package com.beepbeat.AE2XTender;
 
 import com.beepbeat.AE2XTender.item.AE2CreativeCell;
 import com.beepbeat.AE2XTender.utility.LogHelper;
-import net.minecraft.client.Minecraft;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.item.EntityItem;
@@ -32,8 +31,8 @@ public class XPDRCommand extends CommandBase
 
                  if (parameters[0].equals("getSet")) {
                     if (parameters.length >= 2){
-                        if (parameters[1].equals("tc")) {
-                            ItemStack ItemsToDrop = AE2CreativeCell.SetThaumcraft(AE2CreativeCell.getCreCellItemStack());
+                        if (parameters[1].equals("tc") && parameters[2] == null) {
+                            ItemStack ItemsToDrop = AE2CreativeCell.SetThaumcraft(new ItemStack(new AE2CreativeCell()));
                             LogHelper.info("NBT after Command:" + ItemsToDrop.getTagCompound().toString());
 
                             EntityItem dropItem = new EntityItem(player.getEntityWorld(), player.posX, player.posY, player.posZ, ItemsToDrop);
@@ -41,7 +40,12 @@ public class XPDRCommand extends CommandBase
                             player.getEntityWorld().spawnEntityInWorld(dropItem);
 
                             //player.dropItem(ItemsToDrop.getItem().setUnlocalizedName("Creative Cell:tc").setMaxStackSize(1), 1);
-                            player.addChatMessage(new ChatComponentText("Creating Set 'tc'"));
+                            if (parameters[2] != null) {
+                                player.addChatMessage(new ChatComponentText("Creating Set 'tc'"));
+                                ItemStack tmpStack = player.inventory.getStackInSlot(Integer.parseInt(parameters[2]));
+                                player.inventory.addItemStackToInventory(AE2CreativeCell.SetThaumcraft(tmpStack));
+                            }
+
 
                         }
 
